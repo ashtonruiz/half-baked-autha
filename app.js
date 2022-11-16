44c276005f34a5750777ebfabd223ad40cb19921
Ashton Ruiz <ashton.ruiz@yahoo.com>
2022-11-16 11:32:30 -0800

    worked on signUpForm event listener and redirecting to other page if successful login

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
-const signInForm = document.getElementById('sign-in');
+import { redirectIfLoggedIn, signupUser } from './fetch-utils.js';
 
+const signInForm = document.getElementById('sign-in');
 const signUpForm = document.getElementById('sign-up');
 
 // Wire up sign in and sign up forms to supabase
-// Redirect to /other-page on successful auth
-// Redirect to /other-page when page loads if user is authenticated
+signUpForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const data = new FormData(signUpForm);
+    const email = data.get('email');
+    const user = await signupUser(email, data.get('password'));
+    // Redirect to /other-page on successful auth
+    redirectIfLoggedIn();
+    // Redirect to /other-page when page loads if user is authenticated
+    if (user) {
+        location.replace('/other-page');
+    }
+});
