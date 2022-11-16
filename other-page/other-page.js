c611441e75a84b825e714d1748544372049d1c95
Ashton Ruiz <ashton.ruiz@yahoo.com>
2022-11-16 11:46:39 -0800

    worked on logout function and then tested website. You can successfully sign up and Supabase is getting the data and storing it but when you login nothing happens so I am going to work on that next

--- other-page/other-page.js
+++ other-page/other-page.js
@@ -1,5 +1,5 @@
 // use checkAuth function to redirect is user not authenticated
-import { checkAuth } from '../fetch-utils.js';
+import { checkAuth, logout } from '../fetch-utils.js';
 
 checkAuth();
 // add event listener to the logout button and call logout
